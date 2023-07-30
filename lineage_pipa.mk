e099f2f7979312346f5aa988cc5a23cd77f8ea74
pipa-development <pipa.development@gmail.com>
2023-07-30 12:42:32 +0530

    Add Initial Support -Transition

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -11,15 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from alioth device
-$(call inherit-product, device/xiaomi/alioth/device.mk)
+# Inherit from pipa device
+$(call inherit-product, device/xiaomi/pipa/device.mk)
 
-PRODUCT_NAME := lineage_alioth
-PRODUCT_DEVICE := alioth
+PRODUCT_NAME := lineage_pipa
+PRODUCT_DEVICE := pipa
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO F3
+PRODUCT_BRAND := XIAOMI
+PRODUCT_MODEL := PAD 6
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHMIXM:user/release-keys
